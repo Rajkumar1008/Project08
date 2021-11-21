4df3d419836b8ddbcace5efb976726b36d9448d7
Rajkumar1008 <rajkumar1094r@gmail.com>
2021-11-21 11:51:39 +0530

    done

---
+++ src/org/add/GreenTech.java
@@ -0,0 +1,33 @@
+package org.add;
+
+public class GreenTech {
+
+	private void greensOmr() {
+	System.out.println("omr main road");
+	}
+	private void greensAdyar() {
+    System.out.println("adayar main road adayar");
+}
+	private void greensTambaram() {
+		System.out.println("tambaram main road");
+}
+private void greensVelachery() {
+	System.out.println("velachery main block");
+
+}
+private void greensAnnanager() {
+	System.out.println("anna nagar main road");
+}
+public static void main(String[] args) {
+	
+GreenTech cute = new GreenTech();
+cute.greensOmr();
+cute.greensAdyar();
+cute.greensTambaram();
+cute.greensVelachery();
+cute.greensAnnanager();
+}
+	
+
+ 
+}
